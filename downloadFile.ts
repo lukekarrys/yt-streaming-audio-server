c65559827df60daf076f208f676e239ea7247526
Luke Karrys <lukekarrys@gmail.com>
2021-04-15 18:25:31 -0700

    add logs

--- downloadFile.ts
+++ downloadFile.ts
@@ -2,13 +2,15 @@ import execa from 'execa'
 import os from 'os'
 import fs from 'fs-extra'
 import path from 'path'
-import gettmdir from 'temp-dir'
 
-// const tmpdir = os.tmpdir()
-const tmpdir = gettmdir
+const tmpdir = os.tmpdir()
 
 const downloadFile = async (id: string, dest: string) => {
-  await execa('youtube-dl', [
+  const tmpPath = `${tmpdir}/%(id)s.%(ext)s`
+
+  console.log(`Downloading to ${tmpPath}`)
+
+  const { stdout, stderr } = await execa('youtube-dl', [
     '-f',
     'bestaudio',
     '--extract-audio',
@@ -17,9 +19,13 @@ const downloadFile = async (id: string, dest: string) => {
     '--audio-quality',
     '0',
     '-o',
-    `${tmpdir}/%(id)s.%(ext)s`,
+    tmpPath,
     id,
   ])
+
+  console.log('stdout', stdout)
+  console.log('stderr', stderr)
+
   await fs.move(path.join(tmpdir, `${id}.mp3`), path.join(dest, `${id}.mp3`))
 }
 
