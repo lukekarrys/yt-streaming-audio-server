74457f7bdfbc0b80689087d037c61f8973b7a2ef
Luke Karrys <lukekarrys@gmail.com>
2021-04-16 08:44:26 -0700

    download to mp3

--- downloadFile.ts
+++ downloadFile.ts
@@ -1,12 +1,7 @@
 import execa from 'execa'
-import os from 'os'
-import fs from 'fs-extra'
-import path from 'path'
-
-const savedir = os.homedir()
 
 const downloadFile = async (id: string, dest: string) => {
-  const tmpPath = `${savedir}/%(id)s.%(ext)s`
+  const tmpPath = `./mp3/%(id)s.%(ext)s`
 
   console.log(`Downloading to ${tmpPath}`)
 
@@ -23,10 +18,8 @@ const downloadFile = async (id: string, dest: string) => {
     id,
   ])
 
-  console.log('stdout', stdout)
-  console.log('stderr', stderr)
-
-  await fs.move(path.join(savedir, `${id}.mp3`), path.join(dest, `${id}.mp3`))
+  console.log('stdout:', stdout)
+  console.log('stderr:', stderr)
 }
 
 export default downloadFile
